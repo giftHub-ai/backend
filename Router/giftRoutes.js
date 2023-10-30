2a9b40828524a5cfe90f8eabfaf9226d2fcf1fa5
kart027 <kartikgamot2020@gmail.com>
2023-10-30 14:10:37 +0530

    added route

--- Router/giftRoutes.js
+++ Router/giftRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { isAuthenticated } = require("../middleware/auth")
-const { Mygift, getGift,giftStatus}= require("../Controllers/giftControllers")
+const { Mygift, getGift,giftStatus,getReturnedGift}= require("../Controllers/giftControllers")
 
 router.route("/getgift").get(isAuthenticated,getGift)
 router.route("/Mygift").get(isAuthenticated,Mygift)
 router.route("/status/:id").post(isAuthenticated,giftStatus)
+router.route("/ReturnedGift").get(isAuthenticated,getReturnedGift)
 
 
 
